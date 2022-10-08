16bde9422a58a5ac8c2ba2cc4f34f29553f49c0a
Mayela <mancheta@cpp.edu>
2022-10-07 23:56:45 -0700

    Checking rows and columns

--- tictactoe.c
+++ tictactoe.c
@@ -10,6 +10,9 @@ const char NPC ='O';
 
 void printBoard();
 void clearBoard();
+int checkSpaces();
+char checkWin();
+void humanMove();
 
 int main()
 {
@@ -31,6 +34,16 @@ scanf("%d", &gamemode);
     if(gamemode==2)
     {
         clearBoard();
+
+        while(win == ' ' && checkSpaces()!=0)
+        {
+            printBoard();
+
+            humanMove();
+
+            win = checkWin();
+
+        }
         printBoard();
         
     }
@@ -63,4 +76,76 @@ void printBoard()
 
     printf("| %c | %c | %c |", board[2][0], board[2][1], board[2][2]);
     printf("\n+--------+\n");
+}
+
+int checkSpaces()
+{
+    int blankSpaces = 9;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(board[i][j]!=' ')
+            {
+                blankSpaces--;
+            }
+        }
+    }
+    return blankSpaces;
+}
+
+void humanMove()
+{
+    int a;
+    int b;
+    
+    do
+    {
+     printf("Enter designated row (#1-3");
+    scanf("%d, &a");
+    a--;
+
+    printf("Enter designated column (#1-3");
+    scanf("%d, &b");
+    b--;
+
+    if(board[a][b]!=' ')
+    {
+        printf("Error! Space isn't blank\n");
+
+    }
+    else
+    {
+        board[a][b]= HUMAN;
+        break;
+    }
+    } while (board[a][b]!=' ');
+
+    
+}   
+
+char checkWin()
+{
+    //check each row
+    for(int i=0;i<3;i++)
+    {
+        if(board[i][0]==board[i][1] && board[i][0]==board[i][2])
+        {
+            return board[i][0];
+        }
+        
+    }
+    
+    //check columns
+    for(int i=0;i<3;i++)
+    {
+        if(board[0][i]==board[1][i] && board[0][i]==board[2][i])
+        {
+            return board[0][i];
+        }
+        
+    }
+
+    
+
 }
